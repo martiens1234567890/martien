9270071b5a91000a139cd63413cb56c9ac98fa8e
BLACKDOG.INC <marteenprasetyo@gmail.com>
2023-03-02 21:48:22 +0700

    test_trial

---
+++ day1.js
@@ -0,0 +1,28 @@
+// //membuat variable
+// var test1="day1";
+// let test2="day2";
+// const test3="day3";
+
+
+// console.log(test1)
+// console.log(test2)
+// console.log(test3)
+
+// let x=2;
+// let z=x*2;
+// let a=x+2*z
+
+// console.log("pembagian",a)
+// const arrai=["test1","test2","test3"];
+// const obj ={test1:2345, test2:100};
+
+// console.log (arrai[1]);
+// console.log(obj,"testcuy");
+
+let obj0 = {nama :"trial", job : "gabut", hoby: "ramfog"}
+let arr = ["nama",{nama :"trial", job : "gabut", hoby: "ramfog"},12]
+
+console.log (obj0,"job");
+console.log (arr[2]);
+
+
